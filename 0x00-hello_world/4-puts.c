220f9c5dcf236b50a00ffcdd44aeaa14df8f8012
jaroldhakins <stivenhakins@gmail.com>
2021-06-18 03:02:16 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *This program print "Programming is like building a multilingual puzzle 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main ()
+
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+
+  return 0;
+
+}
