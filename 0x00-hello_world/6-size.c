e3ce003108f9d54037c2aa6261997d2e67504ce9
jaroldhakins <stivenhakins@gmail.com>
2021-06-18 18:05:50 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ long int entero_largo;
 long long int entero_extralargo;
 float flotante;
 
-printf("Size of a char: %lu byte(s) \n", sizeof(caracter));
-printf("Size of an int: %lu byte(s) \n", sizeof(entero));
-printf("Size of a long int: %lu byte(s) \n", sizeof(entero_largo));
-printf("Size of a long long int: %lu byte(s) \n", sizeof(entero_extralargo));
-printf("Size of a float: %lu byte(s) \n", sizeof(flotante));
+printf("Size of a char: %lu byte(s)\n", sizeof(caracter));
+printf("Size of an int: %lu byte(s)\n", sizeof(entero));
+printf("Size of a long int: %lu byte(s)\n", sizeof(entero_largo));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(entero_extralargo));
+printf("Size of a float: %lu byte(s)\n", sizeof(flotante));
 
 return (0);
 
