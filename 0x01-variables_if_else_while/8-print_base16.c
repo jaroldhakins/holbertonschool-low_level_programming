eab8460f3fd0f469a9d5ab4c693f559dc87fb586
jaroldhakins <stivenhakins@gmail.com>
2021-06-20 18:00:50 +0000

    ./8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Print numeros hexadecimales
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char n;
+
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+
+for (n = 'a'; n <= 'f'; n++)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+
+}
