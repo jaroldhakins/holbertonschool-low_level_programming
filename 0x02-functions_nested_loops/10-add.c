7bda95b25f4037d7f6fd0d5b1dd9e7dc3d1b1c10
jaroldhakins <stivenhakins@gmail.com>
2021-06-23 03:56:02 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - suma dos numeros y devuelve el resultado
+ *
+ * @num1: number one.
+ * @num2: number two.
+ *
+ * Return: la suma de dos numeros
+ */
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
