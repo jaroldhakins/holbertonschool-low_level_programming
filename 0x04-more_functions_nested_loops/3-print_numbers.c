feb13ac95bf3fee41c2b0766a3488e72c68c0a5b
jaroldhakins <stivenhakins@gmail.com>
2021-06-25 04:08:17 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - print numbers 0 to 9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
