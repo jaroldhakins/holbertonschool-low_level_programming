55d7a7ad4c968b553e1223d8f4c043f2f5f245bf
jaroldhakins <stivenhakins@gmail.com>
2021-06-25 03:45:36 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplica dos enteros
+ *
+ *@a: is a integer
+ *@b: is a integer
+ *@multiplicar: is a integer
+ *
+ * Return: multiplicar
+ */
+
+int mul(int a, int b)
+{
+int multiplicar;
+
+multiplicar = a * b;
+
+return (multiplicar);
+}
