8710f9a16b8a504c836c066b194139e585e3fbba
jaroldhakins <stivenhakins@gmail.com>
2021-06-26 02:50:36 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
@@ -31,7 +31,7 @@ else
 printf("%i ", i);
 }
 }
+printf("Buzz");
 printf("\n");
-
 return (0);
 }
