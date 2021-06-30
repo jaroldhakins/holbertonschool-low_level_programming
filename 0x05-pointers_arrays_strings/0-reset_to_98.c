fa0460d7591835827e9ccc4c3018777dc52487f0
jaroldhakins <stivenhakins@gmail.com>
2021-06-30 01:15:38 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - actualiza el valor de un entero desde un puntero
+ *
+ * @n: es un puntero
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05-pointers_arrays_strings
+
+This project is about pointers and arrays
