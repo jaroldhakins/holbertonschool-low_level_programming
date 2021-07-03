7bee89fc6285ad4abf5d3356757ed4b384ce5994
jaroldhakins <stivenhakins@gmail.com>
2021-07-03 02:29:24 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,12 +17,9 @@ int a, b;
 for (a = 0; dest[a] != 0; a++)
 {
 }
-for (b = 0; src[b]; b++)
-{
-if (b < n && src[b] != 0)
+for (b = 0; b < n && src[b] != 0; b++)
 {
 dest[a] = src[b];
-}
 a++;
 }
 dest[a] = 0;
