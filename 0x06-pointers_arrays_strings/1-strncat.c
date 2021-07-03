4e094fa1bda14e98662116f465e52992aaff8c7b
jaroldhakins <stivenhakins@gmail.com>
2021-07-03 02:13:24 +0000

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *_strncat - concatena dos funciones
+ *
+ *@dest: puntero char
+ *@src: puntero char
+ *@n: es un entero
+ *
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+
+for (i = 0; dest[i]; i++)
+{
+}
+for (j = 0; src[j]; j++)
+{
+if (j != n)
+{
+dest[i] = src[j];
+}
+i++;
+}
+return (dest); 
+}
