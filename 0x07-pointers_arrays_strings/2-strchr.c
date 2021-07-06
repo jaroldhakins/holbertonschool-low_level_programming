b55189168d2ebee196716f3a92b476c5358d33cf
jaroldhakins <stivenhakins@gmail.com>
2021-07-06 01:52:49 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - localiza caracteres en un string
+ * @s: array o string
+ * @c: caracter
+ *
+ * Return: un puntero a un caracter
+ */
+char *_strchr(char *s, char c)
+{
+int a;
+
+for (a = 0; s[a] != 0; a++)
+{
+if (s[a] == c)
+return (&s[a]);
+}
+return (0);
+}
