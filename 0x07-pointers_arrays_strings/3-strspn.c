05107b397fa217225cdcc39c3bb25b53ea22f9bb
jaroldhakins <stivenhakins@gmail.com>
2021-07-06 02:38:09 +0000

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strspn - da el numero de bytes de lon
+ *
+ * @s: string
+ * @accept: puntero
+ *
+ * Return: numero de bytes de lon
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int a, b;
+unsigned int lon = 0;
+
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; accept[b] != '\0' && accept[b] != s[a]; b++)
+{
+if (s[a] == accept[b])
+{
+lon++;
+}
+if (accept[b] == '\0')
+{
+return (lon);
+}
+}
+return (lon);
+}
