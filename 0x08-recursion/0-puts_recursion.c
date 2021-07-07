2eb3d2a2becffb3cd12da238ad12d4798049dbbd
jaroldhakins <stivenhakins@gmail.com>
2021-07-07 23:18:08 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ *
+ *@s: Apunta a la drieccion del string
+ *
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# RECURSION
+
+In this project I'm going to make six exercises to learn and practice Recursion
