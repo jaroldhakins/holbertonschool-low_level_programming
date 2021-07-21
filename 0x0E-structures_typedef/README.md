adcd686dc74ab6406066db44409ec933d1293fd2
jaroldhakins <stivenhakins@gmail.com>
2021-07-21 15:32:36 +0000

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# STRUCTURES AND TYPEDEF
+
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+
+/**
+ * struct dog - Define a new type of structure
+ * with the following elements: name, age, owner.
+ *
+ *@name: name of the pet
+ *@age: age of the pet
+ *@owner: pet's owner
+ *
+ * Return: no return
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
