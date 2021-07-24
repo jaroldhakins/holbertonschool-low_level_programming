2007c037913a08fb19660701aeb318c5cd6b9530
jaroldhakins <stivenhakins@gmail.com>
2021-07-24 21:32:25 +0000

    calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,21 +14,29 @@
 int main(int argc, char *argv[])
 {
 	int i, n1, n2;
+	char *operator;
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 
 	i = get_op_func(argv[2])(n1, n2);
+	operator = argv[2];
 	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (!(get_op_func(argv[2])))
+	if (!(get_op_func(operator)))
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if ((*operator == '/' || *operator == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+		
 	}
 	
 	printf("%i\n", i);
