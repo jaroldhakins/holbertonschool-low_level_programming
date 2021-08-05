23398d00cc7930a4e8260355a23b38a0d3228e95
jaroldhakins <stivenhakins@gmail.com>
2021-08-05 21:58:57 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,12 +17,19 @@ unsigned int binary_to_uint(const char *b)
 	for (i = strlen(b) - 1; b[i]; i--)
 	{
 		if (!b)
+		{
 			return (0);
-		if (b[i] != '0' &&  b[i] != '1')
+		}
+		if (b[i] == '0' ||  b[i] == '1')
+		{
+			entero = (b[i] - 48);
+			total += entero * pot;
+			pot *= 2;
+		}
+		else
+		{
 			return (0);
-		entero = (b[i] - 48);
-		total += entero * pot;
-		pot *= 2;
+		}
 	}
 	return (total);
 }
