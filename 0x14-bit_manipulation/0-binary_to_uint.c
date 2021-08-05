6d9144750ddb4d3b4f2ddb65683464a96ea7ceec
jaroldhakins <stivenhakins@gmail.com>
2021-08-05 21:43:02 +0000

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * binary_to_uint - converts to a binary number to unsigned int
+ * @b: pointer to string
+ * Return: unsigned int total or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int pot = 1;
+	unsigned int total = 0;
+	unsigned int entero = 0;
+
+	for (i = strlen(b) - 1; b[i]; i--)
+	{
+		if (!b)
+			return (0);
+		if (b[i] != '0' &&  b[i] != '1')
+			return (0);
+		entero = (b[i] - 48);
+		total = total + (entero * pot);
+		pot *= 2;
+	}
+	return (total);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef BIT
+#define BIT
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
