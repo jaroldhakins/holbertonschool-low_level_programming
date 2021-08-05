e7287a8a6251883162d985cda27c4bc5be2b53c1
jaroldhakins <stivenhakins@gmail.com>
2021-08-05 21:48:19 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int total = 0;
 	unsigned int entero = 0;
 
-	for (i = strlen(b) - 1; b[i]; i--)
+	for (i = strlen(b) - 1; b[i]; i--, pot *= 2)
 	{
 		if (!b)
 			return (0);
@@ -22,7 +22,6 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		entero = (b[i] - 48);
 		total = total + (entero * pot);
-		pot *= 2;
 	}
 	return (total);
 }
