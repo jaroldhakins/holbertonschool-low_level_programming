7f340d0af65f02a1404a230d6eb8c25e70b96505
jaroldhakins <stivenhakins@gmail.com>
2021-09-25 03:10:27 +0000

    100-operations.so

---
+++ 0x18-dynamic_libraries/basic_operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+    return a + b;
+}
+/**
+ * sub - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int sub(int a, int b)
+{
+    return a - b;
+}
+/**
+ * mul - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int mul(int a, int b)
+{
+    return a * b;
+}
+/**
+ * div - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int div(int a, int b)
+{
+    return a / b;
+}
+/**
+ * mod - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int mod(int a, int b)
+{
+    return a % b;
+}
